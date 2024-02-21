c7fc79247b8b9a914045d02699ab41f2c2aafcd6
jaydwevidi <jayraj1999@gmail.com>
2024-02-21 18:16:29 -0500

    added user registeration function

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, World!' + ``);
+});
+
+app.post('user/register/', (req, res) => {
+    res.send("User Registered Successfully.")
 });
 
 const PORT = 3000;
