e34d5782e8394f39a313bd27c1d687d46e98705b
jaydwevidi <jayraj1999@gmail.com>
2024-03-02 18:02:44 -0500

    added summarize with video id, needs refactoring

--- controllers/summaryController.js
+++ controllers/summaryController.js
@@ -1,60 +1,73 @@
 const axios = require('axios');
 const isTokenValid = require('./tokenValidator');
+const { getSubtitles } = require('youtube-captions-scraper');
 
 exports.summarize = async (req, res) => {
+    
     const open_ai_auth_token = process.env.OPEN_AI_KEY ;
 
-    console.log(open_ai_auth_token);
-    user_token = req.body.user_token
-    if (!user_token) {
-        return res.status(400).json({ error: 'User Token is required' });
-      }
-    
-    const isValid = await isTokenValid(user_token);
-    if (!isValid){
-        return res.status(400).json({ error: 'Invalid Token' });
+    const videoId = req.body.video_id;
+    if (!videoId){
+        return res.status(400).json({ error: 'No Video Id Provided' }); 
     }
 
     try {
-      
-      const transcript = req.body.transcript; // Extracting transcript from the request body
-      const llm_model = req.body.llm_model || "gpt-3.5-turbo-0125";
-  
-      console.log("using model - " + llm_model);
-      const requestBody = {
-        "model": llm_model,
-        "messages": [
-          {
-            "role": "system",
-            "content": "Your only job is to return the summary of a video transcript provided"
-          },
-          {
-            "role": "user",
-            "content": transcript // Using the extracted transcript
-          }
-        ]
-      };
-  
-      const response = await axios.post('https://api.openai.com/v1/chat/completions', requestBody, {
-        headers: {
-          'Authorization': `Bearer ${open_ai_auth_token}`,
-          'Content-Type': 'application/json',
-          // Add any other headers you need here
-        }
-      });
-  
-      // Extracting the content and usage from the response
-      const summaryContent = response.data.choices[0].message.content;
-      const usage = response.data.usage;
-  
-      // Creating a custom response object
-      const customResponse = {
-        summary: summaryContent,
-        usage: usage
-      };
-  
-      res.status(200).send(customResponse); // Sending the custom response
+
+    //console.log(open_ai_auth_token);
+    //user_token = req.body.user_token
+
+    //if (!user_token) {
+    //    return res.status(400).json({ error: 'User Token is required' });
+    //  }
+    
+    //const isValid = await isTokenValid(user_token);
+    //if (!isValid){
+    //    return res.status(400).json({ error: 'Invalid Token' }); 
+    //}
+
+        const subtitles = await getSubtitles({
+            videoID: videoId, // YouTube video ID
+            lang: 'en' // Language for the subtitles
+        });
+        
+        const subtitles_to_send = subtitles.map(subtitle => subtitle.text).join(' ');
+
+        const llm_model = req.body.llm_model || "gpt-3.5-turbo-0125";
+        const summary_word_count = req.body.word_limit || 100;
+
+        console.log(`Using Model - ${llm_model}, Word Limit - ${summary_word_count}`);
+
+        const requestBody = {
+            "model": llm_model,
+            "messages": [
+                {
+                    "role": "system",
+                    "content": `Do your best to summarize a transcript of a video in ${summary_word_count} words. You should cover all important points.`
+                },
+                {
+                    "role": "user",
+                    "content": subtitles_to_send // Using the extracted transcript
+                }
+            ]
+        };
+    
+        const response = await axios.post('https://api.openai.com/v1/chat/completions', requestBody, {
+            headers: {
+                'Authorization': `Bearer ${open_ai_auth_token}`,
+                'Content-Type': 'application/json',
+            }
+        });
+    
+        const summaryContent = response.data.choices[0].message.content;
+        const usage = response.data.usage;
+    
+        const customResponse = {
+            summary: summaryContent,
+            usage: usage
+        };
+    
+        res.status(200).send(customResponse); // Sending the custom response
     } catch (error) {
-      res.status(500).send(error.message);
+        res.status(500).send(error.message);
     }
-  };
+};
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 
+
 const express = require('express');
 const app = express();
+
+
+
+
+
+
+
 
 // Body parsing middleware
 app.use(express.json());
@@ -14,6 +22,13 @@ const summaryRoutes = require('./routes/summaryRoutes');
 // Using routes
 app.use('/users', userRoutes);
 app.use('/summarize', summaryRoutes);
+
+
+app.post('/get-transcript', (req, res) => {
+  const body = req.body;
+  console.log('Received POST request:', body);
+  res.status(200).json({ message: 'POST request received', data: body });
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
