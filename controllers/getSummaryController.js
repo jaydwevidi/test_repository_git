5d4f51451b0fe14f784f82d9a3b8b65575b8633b
jaydwevidi <jayraj1999@gmail.com>
2024-03-03 16:18:13 -0500

    reformat 2

--- controllers/getSummaryController.js
+++ controllers/getSummaryController.js
@@ -6,13 +6,5 @@ exports.getSummary = async (req, res) => {
   const open_ai_auth_token = process.env.OPEN_AI_KEY;
   const videoId = req.body.video_id;
 
-  const addVideoToDbUrl = "http://localhost:3000/addVideoToDb";
-  addVideoToDbResponse = await axios.post(addVideoToDbUrl, {
-    video_id: req.body.video_id,
-    number_of_questions: req.body.number_of_questions,
-  });
-
-  res
-    .status(200)
-    .json({ message: "getSummary Working", ...addVideoToDbResponse });
+  res.status(200).json({ message: "getSummary Working" });
 };
