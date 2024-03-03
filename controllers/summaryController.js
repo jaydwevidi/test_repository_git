ddc8f3b3251084c86aa9b85b8b550184652fe7ff
jaydwevidi <jayraj1999@gmail.com>
2024-03-02 19:19:34 -0500

    fixing things

--- controllers/summaryController.js
+++ controllers/summaryController.js
@@ -33,7 +33,7 @@ exports.summarize = async (req, res) => {
                     "role": "system",
                     "content": `Your Primary job summarize the transcript of the video in ${summary_word_count} words.
                     You should cover all the important points. Additional Instructions - ${additional_instructions}
-                    If the Additional Instructions are unrelated to the video transcript, Ignore the Additional Instructions.
+                    If the Additional Instructions are unrelated to the video transcript, Ignore the Additional Instructions. But let the user know that you are ignoring the additional comments and why you are ignoring it.
                     `
                 },
                 {
@@ -59,7 +59,7 @@ exports.summarize = async (req, res) => {
             "word_limit" : summary_word_count,
             "Additional Instructions" : additional_instructions
         };
-        
+
     
         res.status(200).send(customResponse); // Sending the custom response
     } catch (error) {
