da6602ed4ef39d7b2e4c2ec279d80f850321dc0a
jaydwevidi <jayraj1999@gmail.com>
2024-03-03 04:13:08 -0500

    refactoring 5

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 
+
 const express = require('express');
 const app = express();
+
 
 // Body parsing middleware
 app.use(express.json());
@@ -10,23 +12,20 @@ app.use(express.urlencoded({ extended: true }));
 // Importing routes
 const userRoutes = require('./routes/userRoutes');
 const summaryRoutes = require('./routes/summaryRoutes');
-const mcqRoutes = require('./routes/getMcqRoutes');
-const addVideoToDbRoutes = require('./routes/addVideoToDbRoutes');
+const getMcqRoutes = require('./routes/getMcq')
 
 // Using routes
 app.use('/users', userRoutes);
 app.use('/summarize', summaryRoutes);
-app.use('/getMcq', mcqRoutes);
-app.use('/addVideoToDb', addVideoToDbRoutes);
+app.use('/getMcq', getMcqRoutes)
 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
 
-// Start the server
-const PORT = process.env.PORT || 3000;
+
+const addVideoToDbController = require('./routes/addVideoToDbRoutes')
+app.use('/addVideoToDb' , addVideoToDbController)
+
+
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
