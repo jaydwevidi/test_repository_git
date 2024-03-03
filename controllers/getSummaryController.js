900f77f69171f4eefb3d4c654b27b951b75b29ca
jaydwevidi <jayraj1999@gmail.com>
2024-03-03 16:41:51 -0500

    reformat 2

--- controllers/getSummaryController.js
+++ controllers/getSummaryController.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-//const isTokenValid = require('./tokenValidator');
 const { getSubtitles } = require("youtube-captions-scraper");
 
 exports.getSummary = async (req, res) => {
@@ -9,14 +8,22 @@ exports.getSummary = async (req, res) => {
 
   const addVideoToDbUrl = "http://localhost:3000/addVideoToDb";
 
-  addVideoToDbResponse = await axios.post(addVideoToDbUrl, {
-    video_id: req.body.video_id,
-    number_of_questions: req.body.number_of_questions,
-  });
+  try {
+    const addVideoToDbResponse = await axios.post(addVideoToDbUrl, {
+      video_id: req.body.video_id,
+      number_of_questions: req.body.number_of_questions,
+    });
+    console.log("Response from addVideoToDb:", addVideoToDbResponse.data);
 
-  console.log("\n\n\n\n\n\n " + addVideoToDbResponse);
+    // Define summaryContent and usage variables here
 
-  res.status(200).json({
-    respon: addVideoToDbResponse,
-  });
+    res.status(200).json({
+      questions: summaryContent,
+      usage: usage,
+      response: addVideoToDbResponse.data,
+    });
+  } catch (error) {
+    console.error("Error in addVideoToDb request:", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
