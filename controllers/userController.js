71ee266dd5f79c90850d4e5f441e05c4e976cded
jaydwevidi <jayraj1999@gmail.com>
2024-03-04 18:48:39 -0500

    update

--- controllers/userController.js
+++ controllers/userController.js
@@ -97,7 +97,7 @@ exports.getUserDetails = async (req, res) => {
 
     const userData = users[0];
 
-    res.json({ name: userData.name, email: userData.email });
+    res.json({ name: userData, email: userData.email });
   } catch (error) {
     res.status(500).json({
       message: "Error fetching user details",
