75b3465d696b0dc2b8b202d6652695e313801d60
jaydwevidi <jayraj1999@gmail.com>
2024-03-05 18:53:55 -0500

    updated register gender bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,7 +23,7 @@ exports.register = async (req, res) => {
 
     const [result] = await pool.query(
       "INSERT INTO users (fname, lname, email, password, phone, gender, dob) VALUES (?, ?, ?, ?, ?, ?, ?)",
-      [fname, lname, email, hashedPassword, phone, gender, dob]
+      [fname, lname, email, hashedPassword, phone, convertGender(gender), dob]
     );
 
     res
@@ -61,7 +61,7 @@ exports.login = async (req, res) => {
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid email or password" });
+      return res.status(401).json({ message: "Invalid password" });
     }
 
     // Create a JWT token
@@ -105,3 +105,39 @@ exports.getUserDetails = async (req, res) => {
     });
   }
 };
+
+function convertGender(gender) {
+  // Convert gender to lowercase for case-insensitive comparison
+  const genderLower = gender.toLowerCase();
+
+  // Assign a number to each gender
+  // 1 = Male, 2 = Female, 3 = Non-Binary, 4 = Transgender, 5 = Intersex, 6 = Prefer Not to Say, 7 = Other
+  switch (genderLower) {
+    case "male":
+    case "m":
+      return 1;
+    case "female":
+    case "f":
+      return 2;
+    case "non-binary":
+    case "nonbinary":
+    case "nb":
+      return 3;
+    case "transgender":
+    case "trans":
+    case "tg":
+      return 4;
+    case "intersex":
+    case "i":
+      return 5;
+    case "prefer not to say":
+    case "pnts":
+      return 6;
+    case "other":
+    case "o":
+      return 7;
+    default:
+      // If gender is not recognized, return null
+      return null;
+  }
+}
