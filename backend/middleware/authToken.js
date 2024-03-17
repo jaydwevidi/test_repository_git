80ae6c39db9bea3f3a1c9f5efb65bc6f531ca69a
jaydwevidi <jayraj1999@gmail.com>
2024-03-17 05:06:33 -0400

    updates

--- backend/middleware/authToken.js
+++ backend/middleware/authToken.js
@@ -6,9 +6,10 @@ const authenticateToken = (req, res, next) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  console.log(`\nAuthenticating token - ${token}\n`);
+  console.log(`\nVerifying Authenticating token - ${token}\n`);
 
   if (!token) {
+    console.log(`Token not Provided.`);
     return res.status(401).json({ message: "No token provided" });
   }
 
@@ -16,8 +17,10 @@ const authenticateToken = (req, res, next) => {
     const decoded = jwt.verify(token, JWT_SECRET_KEY);
 
     req.body.user_id = decoded.userId; // Add User Id to req.body
+    console.log(`Token is Authentic`);
     next();
   } catch (error) {
+    console.log(`Token is Invalid`);
     res.status(403).json({ message: "Invalid token" });
   }
 };
